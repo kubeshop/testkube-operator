1c258a530125fd633a9592cea277a1a354a78959
Jacek Wysocki <jacek.wysocki@gmail.com>
2021-12-07 09:28:56 +0100

    feat: GetClient with error

--- client/client.go
+++ client/client.go
@@ -1,8 +1,6 @@
 package client
 
 import (
-	"log"
-
 	executorv1 "github.com/kubeshop/testkube-operator/apis/executor/v1"
 	scriptv1 "github.com/kubeshop/testkube-operator/apis/script/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -12,19 +10,12 @@ import (
 )
 
 // GetClient returns kubernetes CRD client with registered schemes
-// TODO I don't like thoses deps - rethink structure and refactor
-// client is included in ScriptClient (but client need to know about script :/)
-func GetClient() client.Client {
+func GetClient() (client.Client, error) {
 	scheme := runtime.NewScheme()
 
 	scriptv1.AddToScheme(scheme)
 	executorv1.AddToScheme(scheme)
 
 	kubeconfig := ctrl.GetConfigOrDie()
-	controllerClient, err := client.New(kubeconfig, client.Options{Scheme: scheme})
-	if err != nil {
-		log.Fatal(err)
-		return nil
-	}
-	return controllerClient
+	return client.New(kubeconfig, client.Options{Scheme: scheme})
 }
