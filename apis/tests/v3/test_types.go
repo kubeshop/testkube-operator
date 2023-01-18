2bea1c5ca280064963ef935211d81962aa9c13c7
nicufk <nicolae@kubeshop.io>
2023-01-18 16:44:56 +0200

    fix: cert json typo

--- apis/tests/v3/test_types.go
+++ apis/tests/v3/test_types.go
@@ -83,7 +83,7 @@ type Repository struct {
 	Path              string     `json:"path,omitempty"`
 	UsernameSecret    *SecretRef `json:"usernameSecret,omitempty"`
 	TokenSecret       *SecretRef `json:"tokenSecret,omitempty"`
-	CertificateSecret string     `json:"certificate,omitempty"`
+	CertificateSecret string     `json:"certificateSecret,omitempty"`
 	// if provided we checkout the whole repository and run test from this directory
 	WorkingDir string `json:"workingDir,omitempty"`
 }
