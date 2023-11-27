abd682a0e8e4938df1decb6e5481c57dbfd40560
Dawid Rusnak <dawid@drcode.pl>
2023-11-27 16:13:34 +0100

    feat: add initial Workflow CRDs

---
+++ api/workflows/v1beta1/workflow_types.go
@@ -0,0 +1,69 @@
+/*
+Copyright 2021.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1beta1
+
+import (
+	"encoding/json"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+type WorkflowTemplateRef struct {
+	Name string `json:"name"`
+	// +kubebuilder:validation:Schemaless
+	Config map[string]*json.RawMessage `json:"config,omitempty"`
+}
+
+// WorkflowSpec defines the desired state of Workflow
+type WorkflowSpec struct {
+	// Important: Run "make" to regenerate code after modifying this file
+
+	Content  *Content             `json:"content,omitempty"`
+	Template *WorkflowTemplateRef `json:"template,omitempty"`
+	Schedule string               `json:"schedule,omitempty"`
+
+	ContainerConfig `json:",inline"`
+
+	Before []Step `json:"before,omitempty"`
+	Steps  []Step `json:"steps,omitempty"`
+	After  []Step `json:"after,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// Workflow is the Schema for the workflows API
+type Workflow struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec WorkflowSpec `json:"spec,omitempty"`
+}
+
+//+kubebuilder:object:root=true
+
+// WorkflowList contains a list of Workflow
+type WorkflowList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []Workflow `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&Workflow{}, &WorkflowList{})
+}
