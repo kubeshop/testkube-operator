c1770f0bea80a2a4f01e833f22d63ba939fa0b36
Dawid Rusnak <dawid@drcode.pl>
2024-02-21 14:40:11 +0100

    feat: add CRD manifests for the future TestWorkflow schema (#216)

---
+++ api/testworkflows/v1/base_types.go
@@ -0,0 +1,20 @@
+package v1
+
+type TestWorkflowSpecBase struct {
+	// Important: Run "make" to regenerate code after modifying this file
+
+	// make the instance configurable with some input data for scheduling it
+	Config map[string]ParameterSchema `json:"config,omitempty"`
+
+	// global content that should be fetched into all containers
+	Content *Content `json:"content,omitempty"`
+
+	// defaults for the containers for all the TestWorkflow steps
+	Container *ContainerConfig `json:"container,omitempty"`
+
+	// configuration for the scheduled job
+	Job *JobConfig `json:"job,omitempty"`
+
+	// configuration for the scheduled pod
+	Pod *PodConfig `json:"pod,omitempty"`
+}
