cf41683a4f12bebe7de57b3bab0ad541a917889d
Dawid Rusnak <dawid@drcode.pl>
2024-02-21 14:40:11 +0100

    feat: add CRD manifests for the future TestWorkflow schema (#216)

---
+++ api/testworkflows/v1/content_types.go
@@ -0,0 +1,47 @@
+package v1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+
+	testsv3 "github.com/kubeshop/testkube-operator/api/tests/v3"
+)
+
+type ContentGit struct {
+	// uri for the Git repository
+	Uri string `json:"uri,omitempty"`
+	// branch, commit or a tag name to fetch
+	Revision string `json:"revision,omitempty"`
+	// plain text username to fetch with
+	Username string `json:"username,omitempty"`
+	// external username to fetch with
+	UsernameFrom *corev1.EnvVarSource `json:"usernameFrom,omitempty"`
+	// plain text token to fetch with
+	Token string `json:"token,omitempty"`
+	// external token to fetch with
+	TokenFrom *corev1.EnvVarSource `json:"tokenFrom,omitempty"`
+	// authorization type for the credentials
+	AuthType testsv3.GitAuthType `json:"authType,omitempty"`
+	// where to mount the fetched repository contents (defaults to "repo" directory in the data volume)
+	MountPath string `json:"mountPath,omitempty"`
+	// paths to fetch for the sparse checkout
+	Paths []string `json:"paths,omitempty"`
+}
+
+type ContentFile struct {
+	// path where the file should be accessible at
+	// +kubebuilder:validation:MinLength=1
+	Path string `json:"path"`
+	// plain-text content to put inside
+	Content string `json:"content,omitempty"`
+	// external source to use
+	ContentFrom *corev1.EnvVarSource `json:"contentFrom,omitempty"`
+	// mode to use for the file
+	Mode *int32 `json:"mode,omitempty"`
+}
+
+type Content struct {
+	// git repository details
+	Git *ContentGit `json:"git,omitempty"`
+	// files to load
+	Files []ContentFile `json:"files,omitempty"`
+}
---
+++ api/testworkflows/v1/types.go
@@ -0,0 +1,70 @@
+package v1
+
+import (
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+)
+
+type Expression string
+
+type ContainerConfig struct {
+	// override default working directory in the image (empty string to default WORKDIR for the image)
+	WorkingDir *string `json:"workingDir,omitempty"`
+
+	// image to be used for the container
+	Image string `json:"image,omitempty"`
+
+	// pulling policy for the image
+	ImagePullPolicy corev1.PullPolicy `json:"imagePullPolicy,omitempty"`
+
+	// environment variables to append to the container
+	Env []corev1.EnvVar `json:"env,omitempty"`
+
+	// external environment variables to append to the container
+	EnvFrom []corev1.EnvFromSource `json:"envFrom,omitempty"`
+
+	// override default command in the image (empty string to default ENTRYPOINT of the image)
+	Command *[]string `json:"command,omitempty"`
+
+	// override default command in the image (empty string to default CMD of the image)
+	Args *[]string `json:"args,omitempty"`
+
+	// expected resources for the container
+	Resources *Resources `json:"resources,omitempty"`
+
+	// security context for the container
+	SecurityContext *corev1.SecurityContext `json:"securityContext,omitempty"`
+}
+
+type Resources struct {
+	// resource limits for the container
+	Limits map[corev1.ResourceName]intstr.IntOrString `json:"limits,omitempty"`
+
+	// resource requests for the container
+	Requests map[corev1.ResourceName]intstr.IntOrString `json:"requests,omitempty"`
+}
+
+type JobConfig struct {
+	// labels added to the scheduled job
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// annotations added to the scheduled job
+	Annotations map[string]string `json:"annotations,omitempty"`
+}
+
+type PodConfig struct {
+	// default service account name for the scheduled pod
+	ServiceAccountName string `json:"serviceAccountName,omitempty"`
+
+	// references to secrets with credentials for pulling the images from registry
+	ImagePullSecrets []corev1.LocalObjectReference `json:"imagePullSecrets,omitempty"`
+
+	// node selector to define on which node the pod should land
+	NodeSelector map[string]string `json:"nodeSelector,omitempty"`
+
+	// labels added to the scheduled pod
+	Labels map[string]string `json:"labels,omitempty"`
+
+	// annotations added to the scheduled pod
+	Annotations map[string]string `json:"annotations,omitempty"`
+}
