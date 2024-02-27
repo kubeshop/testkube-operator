1852257ea4a7bb5df11822df17a8574df87327ec
Dawid Rusnak <dawid@drcode.pl>
2024-02-27 09:34:25 +0100

    feat(TKC-1466): define initial expressions language tags schema in the TestWorkflow objects (#221)

--- api/testworkflows/v1/content_types.go
+++ api/testworkflows/v1/content_types.go
@@ -8,31 +8,31 @@ import (
 
 type ContentGit struct {
 	// uri for the Git repository
-	Uri string `json:"uri,omitempty"`
+	Uri string `json:"uri,omitempty" expr:"template"`
 	// branch, commit or a tag name to fetch
-	Revision string `json:"revision,omitempty"`
+	Revision string `json:"revision,omitempty" expr:"template"`
 	// plain text username to fetch with
-	Username string `json:"username,omitempty"`
+	Username string `json:"username,omitempty" expr:"template"`
 	// external username to fetch with
 	UsernameFrom *corev1.EnvVarSource `json:"usernameFrom,omitempty"`
 	// plain text token to fetch with
-	Token string `json:"token,omitempty"`
+	Token string `json:"token,omitempty" expr:"template"`
 	// external token to fetch with
 	TokenFrom *corev1.EnvVarSource `json:"tokenFrom,omitempty"`
 	// authorization type for the credentials
-	AuthType testsv3.GitAuthType `json:"authType,omitempty"`
+	AuthType testsv3.GitAuthType `json:"authType,omitempty" expr:"template"`
 	// where to mount the fetched repository contents (defaults to "repo" directory in the data volume)
-	MountPath string `json:"mountPath,omitempty"`
+	MountPath string `json:"mountPath,omitempty" expr:"template"`
 	// paths to fetch for the sparse checkout
-	Paths []string `json:"paths,omitempty"`
+	Paths []string `json:"paths,omitempty" expr:"template"`
 }
 
 type ContentFile struct {
 	// path where the file should be accessible at
 	// +kubebuilder:validation:MinLength=1
-	Path string `json:"path"`
+	Path string `json:"path" expr:"template"`
 	// plain-text content to put inside
-	Content string `json:"content,omitempty"`
+	Content string `json:"content,omitempty" expr:"template"`
 	// external source to use
 	ContentFrom *corev1.EnvVarSource `json:"contentFrom,omitempty"`
 	// mode to use for the file
@@ -41,7 +41,7 @@ type ContentFile struct {
 
 type Content struct {
 	// git repository details
-	Git *ContentGit `json:"git,omitempty"`
+	Git *ContentGit `json:"git,omitempty" expr:"include"`
 	// files to load
-	Files []ContentFile `json:"files,omitempty"`
+	Files []ContentFile `json:"files,omitempty" expr:"include"`
 }
