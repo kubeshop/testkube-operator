845d299f49bc35c8703d414186dce6699d4e95f1
Vladislav Sukhin <vladislav@kubeshop.io>
2024-05-23 16:24:24 +0300

    feat: test workflow execution client

--- internal/controller/testworkflowexecution/testsuiteexecution_controllller.go
+++ internal/controller/testworkflowexecution/testsuiteexecution_controllller.go
@@ -42,9 +42,9 @@ type TestWorkflowExecutionReconciler struct {
 	ServicePort int
 }
 
-//+kubebuilder:rbac:groups=tests.testkube.io,resources=testworkflowexecutions,verbs=get;list;watch;create;update;patch;delete
-//+kubebuilder:rbac:groups=tests.testkube.io,resources=testworkflowexecutions/status,verbs=get;update;patch
-//+kubebuilder:rbac:groups=tests.testkube.io,resources=testworkflowexecutions/finalizers,verbs=update
+//+kubebuilder:rbac:groups=testworkflows.testkube.io,resources=testworkflowexecutions,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=testworkflows.testkube.io,resources=testworkflowexecutions/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=testworkflows.testkube.io,resources=testworkflowexecutions/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
