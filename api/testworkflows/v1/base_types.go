1ad791bc55c9623013d3140a8c0505af0c5711c2
Dawid Rusnak <dawid@drcode.pl>
2024-08-14 16:46:55 +0200

    feat: add options to control Test Workflow' containers isolation (#293) (#294)
    * feat: add options to control Test Workflow' containers isolation
    * fix: make TestWorkflowSystem variables optional

--- api/testworkflows/v1/base_types.go
+++ api/testworkflows/v1/base_types.go
@@ -6,6 +6,9 @@ type TestWorkflowSpecBase struct {
 	// events triggering execution of the test workflow
 	Events []Event `json:"events,omitempty" expr:"include"`
 
+	// system configuration to define the orchestration behavior
+	System *TestWorkflowSystem `json:"system,omitempty" expr:"include"`
+
 	// make the instance configurable with some input data for scheduling it
 	Config map[string]ParameterSchema `json:"config,omitempty" expr:"include"`
 
@@ -24,4 +27,12 @@ type TestWorkflowSpecBase struct {
 	// configuration for notifications
 	// Deprecated: field is not used
 	Notifications *NotificationsConfig `json:"notifications,omitempty" expr:"include"`
+}
+
+type TestWorkflowSystem struct {
+	// assume all the steps are pure by default
+	PureByDefault *bool `json:"pureByDefault,omitempty"`
+
+	// disable the behavior of merging multiple operations in a single container
+	IsolatedContainers *bool `json:"isolatedContainers,omitempty"`
 }
