084b41b4a2d68aa92d8773eef5c296a4484e5d59
Vladislav Sukhin <vladislav@kubeshop.io>
2025-04-08 17:33:32 +0300

    feat: add env global flag
    Signed-off-by: Vladislav Sukhin <vladislav@kubeshop.io>

--- api/testworkflows/v1/types.go
+++ api/testworkflows/v1/types.go
@@ -18,7 +18,7 @@ type ContainerConfig struct {
 	ImagePullPolicy corev1.PullPolicy `json:"imagePullPolicy,omitempty" expr:"template"`
 
 	// environment variables to append to the container
-	Env []corev1.EnvVar `json:"env,omitempty" expr:"force"`
+	Env []Env `json:"env,omitempty" expr:"force"`
 
 	// external environment variables to append to the container
 	EnvFrom []corev1.EnvFromSource `json:"envFrom,omitempty" expr:"force"`
@@ -45,6 +45,12 @@ type Resources struct {
 
 	// resource requests for the container
 	Requests map[corev1.ResourceName]intstr.IntOrString `json:"requests,omitempty" expr:"template,template"`
+}
+
+type Env struct {
+	corev1.EnvVar `json:",inline" expr:"include"`
+	// whether to use env var in underlying services
+	Global *bool `json:"global,omitempty" expr:"template"`
 }
 
 type JobConfig struct {
