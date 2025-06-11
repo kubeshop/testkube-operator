efa42c0d7b4bbf1bd2b400b97d50888cc43260f3
Alisdair MacLeod <git@alisdairmacleod.co.uk>
2025-06-11 10:50:26 +0100

    Add new CRD field to specify concurrency handling for workflows

--- api/testworkflows/v1/base_types.go
+++ api/testworkflows/v1/base_types.go
@@ -30,7 +30,35 @@ type TestWorkflowSpecBase struct {
 
 	// values to be used for test workflow execution
 	Execution *TestWorkflowTagSchema `json:"execution,omitempty" expr:"include"`
+
+	// Specifies how to treat concurrent executions of a Workflow.
+	// Valid values are:
+	//
+	// - "Allow" (default): allows Workflows to execute concurrently;
+	// - "Forbid": forbids concurrent executions, rejecting next execution if previous execution hasn't finished yet;
+	// - "Replace": cancels currently running Workflow and replaces it with a new one
+	// +optional
+	ConcurrencyPolicy ConcurrencyPolicy `json:"concurrencyPolicy,omitempty"`
 }
+
+// ConcurrencyPolicy describes how the workflow will be handled.
+// Only one of the following concurrent policies may be specified.
+// If none of the following policies is specified, the default one
+// is AllowConcurrent.
+// +enum
+type ConcurrencyPolicy string
+
+const (
+	// AllowConcurrent allows Workflows to execute concurrently.
+	AllowConcurrent ConcurrencyPolicy = "Allow"
+
+	// ForbidConcurrent disables concurrent executions, rejecting next execution if previous
+	// hasn't finished yet.
+	ForbidConcurrent ConcurrencyPolicy = "Forbid"
+
+	// ReplaceConcurrent cancels currently running Workflow and replaces it with a new one.
+	ReplaceConcurrent ConcurrencyPolicy = "Replace"
+)
 
 type TestWorkflowSystem struct {
 	// assume all the steps are pure by default
